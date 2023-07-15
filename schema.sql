733de8240247b3d8a78e1d5396a64731166b9229
kimitaWanjohi <kimitawanjohi7923@gmail.com>
2023-07-15 07:44:39 +0300

    Add specialization and visits tables

--- schema.sql
+++ schema.sql
@@ -26,3 +26,27 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
 
 ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+  vet_id INTEGER,
+  species_id INTEGER,
+  PRIMARY KEY (vet_id, species_id),
+  FOREIGN KEY (vet_id) REFERENCES vets (id),
+  FOREIGN KEY (species_id) REFERENCES species (id)
+);
+
+CREATE TABLE visits (
+  animal_id INTEGER,
+  vet_id INTEGER,
+  visit_date DATE,
+  PRIMARY KEY (animal_id, vet_id, visit_date),
+  FOREIGN KEY (animal_id) REFERENCES animals (id),
+  FOREIGN KEY (vet_id) REFERENCES vets (id)
+);
